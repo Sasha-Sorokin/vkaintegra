7225979af6118f12e4add8a7b5295376de82d6df
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-09-25 20:17:07 +0700

    chore: bump version to 1.4.0

--- vkaintegra.user.js
+++ vkaintegra.user.js
@@ -4,7 +4,7 @@
 // @author Sasha Sorokin
 // @homepage https://github.com/Sasha-Sorokin/vkaintegra
 // @supportURL https://github.com/Sasha-Sorokin/vkaintegra/issues
-// @version  1.3.2
+// @version  1.4.0
 // @updateURL https://raw.githubusercontent.com/Sasha-Sorokin/vkaintegra/master/vkaintegra.user.js
 // @grant GM.notification
 // @grant GM_notification
